f4957ba1761febe157500bb1e231dad6f6213073
r.tav <rezaanrp@yahoo.com>
2026-06-22 10:56:25 +0330

    Refactor page titles and script sections across various views for consistency and clarity
    - Updated titles in multiple views to provide better context and localization.
    - Changed script section names from "script" to "Scripts" for uniformity.
    - Improved layout titles to include application name for better branding.
    - Enhanced user experience by adding a "Remember Me" checkbox in the login form.
    - Removed unnecessary profile name sections from several views.
    - Adjusted CSS styles for better responsiveness and visual appeal in the admin layout.

--- Server/HamyarBaskoul-main/WebUI/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Server/HamyarBaskoul-main/WebUI/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -88,7 +88,7 @@ namespace WebUI.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password,true /*Input.RememberMe*/, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
 
